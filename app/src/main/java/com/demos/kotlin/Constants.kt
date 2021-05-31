0822ddb2ec4f7a4faa7e9efdc2936b1e4529bed5
YNParis <ynparis@163.com>
2021-05-31 10:15:08 +0800

    TEST TOKEN.

--- app/src/main/java/com/demos/kotlin/Constants.kt
+++ app/src/main/java/com/demos/kotlin/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
 
     companion object {
         const val NEW_VERSION_URL = "http://59.110.162.30/app_updater_version.json"
+        const val lOG_TAG = "KOTLIN_PRACTICE_LOG"
     }
 }
